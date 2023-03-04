b34b41e456dae1ed75b7622068db649da6095dbe
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-03-03 23:33:23 -0400

    Probando usb

--- usb_log.sh
+++ usb_log.sh
@@ -3,3 +3,5 @@
 echo "############################################################################################" >> ~/log_usb.txt
 echo "PUERTO |  FECHA/HORA CONEXION | FECHA/HORA DESCONEXION" >> ~/log_usb.txt 
 date >> ~/log_usb.txt; echo ""  >> ~/log_usb.txt; ls -lhA --time-style=+%d-%m-%Y-%T | grep usb | awk '{print $9,$6,$7}'>> ~/log_usb.txt; echo ""  >> ~/log_usb.txt
+
+#No se que hacer aqui xDDDD
