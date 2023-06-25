59bbd226f24459774fae569db3b70e10d57feac5
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-06-25 13:50:00 -0400

    Arreglando usb_log

--- Tarea1/usb_log.sh
+++ Tarea1/usb_log.sh
@@ -1,100 +1,91 @@
 #!/bin/bash
-  
-if [ -f ~/.log_usb.txt ]; then
-  touch -a ~/.log_usb.txt
+
+FILE="/home/cuevasrja/.log_usb.txt"
+TEMP="/home/cuevasrja/.log_usb.temp"
+TEMP2="/home/cuevasrja/.temp"
+UDEV="/etc/udev/rules.d/usb-change.rules"
+
+if [ -f $FILE ]; then
+  touch -a /tmp/.log_usb.txt
 else
-  touch ~/.log_usb.txt
+  touch $FILE
 fi
 
-if [ -f ~/.temp ]; then
-  rm -f ~/.temp
+if [ -f $TEMP2 ]; then
+  rm -f $TEMP2
 fi
 
-if [ $# -eq 0 ]; then
-  echo -e "Comando invalido: Por favor escriba correctamente.\nEn caso de necesitar ayuda escriba: usb_log --help o usb_log -h"
-elif [ "$1" = "--run" -o "$1" = "-r" ]; then
-  while [ true ]; do
-    if [ $(wc -l ~/.log_usb.txt | cut -d " " -f 1) -eq 0 ]; then
+if [ "$1" = "--run" -o "$1" = "-r" -o $# -eq 0 ]; then
+    if [ $(wc -l $FILE | cut -d " " -f 1) -eq 0 ]; then
         lsusb | while read line; do
-            echo $line > ~/.log_usb.temp
-            ID=$(cut -d " " -f 6 ~/.log_usb.temp)
-            DATE=$(date +'%d/%m/%Y %H:%M:%S')
-            echo "$ID | $DATE | Conectado" >> ~/.log_usb.txt
-            echo -e "\nSe conecto $ID"
-        done
-    elif [ $(($(wc -l ~/.log_usb.txt | cut -d " " -f 1))) -lt $(($(lsusb | wc -l))) ]; then
+        echo $line > $TEMP
+        ID=$(cut -d " " -f 6 $TEMP)
+        DATE=$(date +'%d/%m/%Y %H:%M:%S')
+        echo "$ID | $DATE | Conectado" >> $FILE
+    done
+    elif [ $(($(wc -l $FILE | cut -d " " -f 1))) -lt $(($(lsusb | wc -l))) ]; then
         lsusb | while read line; do
-            echo $line > ~/.log_usb.temp
-            ID=$(cut -d " " -f 6 ~/.log_usb.temp)
-            if $(grep -q "$ID" ~/.log_usb.txt); then
+            echo $line > $TEMP
+            ID=$(cut -d " " -f 6 $TEMP)
+            if $(grep -q "$ID" $FILE); then
                 continue
             else
                 DATE=$(date +'%d/%m/%Y %H:%M:%S')
-                echo "$ID | $DATE | Conectado" >> ~/.log_usb.txt
-                echo -e "\nSe conecto $ID"
+                echo "$ID | $DATE | Conectado" >> $FILE
             fi
         done
     else
-        cat ~/.log_usb.txt | while read line; do
-            echo $line > ~/.log_usb.temp
-            ID=$(cut -d " " -f 1 ~/.log_usb.temp)
-            ESTADO=$(cut -d " " -f 6 ~/.log_usb.temp)
+        cat $FILE | while read line; do
+            echo $line > $TEMP
+            ID=$(cut -d " " -f 1 $TEMP)
+            ESTADO=$(cut -d " " -f 6 $TEMP)
             if $(lsusb | grep -q "$ID"); then
                 if [ "$ESTADO" = "Desconectado" ]; then
                     DATE=$(date +'%d/%m/%Y %H:%M:%S')
-                    LINE=$(grep -n "$ID" ~/.log_usb.txt | cut -d ":" -f 1)
-                    sed -i "${LINE}d" ~/.log_usb.txt
-                    echo "$ID | $DATE | Conectado" >> ~/.log_usb.txt
-                    echo -e "\nSe conecto $ID"
+                    LINE=$(grep -n "$ID" $FILE | cut -d ":" -f 1)
+                    sed -i "${LINE}d" $FILE
+                    echo "$ID | $DATE | Conectado" >> $FILE
                 fi
             else
                 if [ "$ESTADO" = "Conectado" ]; then
                     DATE=$(date +'%d/%m/%Y %H:%M:%S')
-                    LINE=$(grep -n "$ID" ~/.log_usb.txt | cut -d ":" -f 1)
-                    sed -i "${LINE}d" ~/.log_usb.txt
-                    echo "$ID | $DATE | Desconectado" >> ~/.log_usb.txt
-                    echo -e "\nSe desconecto $ID"
+                    LINE=$(grep -n "$ID" $FILE | cut -d ":" -f 1)
+                    sed -i "${LINE}d" $FILE
+                    echo "$ID | $DATE | Desconectado" >> $FILE
                 fi
             fi
         done
     fi
-  done
-  rm -f ~/.log_usb.temp
-elif [ "$1" = "--kill" -o "$1" = "-k" ]; then
-    ps | grep -e "usb_log.sh" > ~/.temp
-    if [ $(wc -l ~/.temp | cut -d " " -f 1) -eq 0 ]; then
-      echo "No hay procesos ejecutandose en segundo plano"
-    else
-      ID=$(head -n 1 ~/.temp | grep -v $$ | cut -d " " -f 2)
-      kill $ID && echo "Se detuvo el proceso de log de puertos USB"
-    fi
-    rm ~/.temp
-    exit
+    rm -f $TEMP
 elif [ "$1" = "--show" -o "$1" = "-s" ]; then
     echo "############################################################################################"
     echo -e "\033[33m    ID    | FECHA/HORA CONEXION | ESTADO\033[0m"
-    cat ~/.log_usb.txt
-    LINES=$(wc -l ~/.log_usb.txt | cut -d " " -f 1)
+    cat $FILE
+    LINES=$(wc -l $FILE | cut -d " " -f 1)
     echo -e "\n\033[33mTotal de registros: $LINES\033[0m"
     echo -e "\033[33mUltima actualizacion: $(date +'%d/%m/%Y %H:%M:%S')\033[0m"
-    echo "El listado de puertos se encuentra en ~/.log_usb.txt"
+    echo "El listado de puertos se encuentra en $FILE"
 elif [ "$1" = "--delete" -o "$1" = "-d" ]; then
-    ps | grep -e "usb_log.sh" > ~/.temp
-    if $(grep -qv "$$" ~/.temp); then
-      echo "Advertencia: Hay que detener el proceso primero"
+    ps | grep -e "usb_log.sh" > $TEMP2
+    if $(grep -qv "$$" $TEMP2); then
+        echo "Advertencia: Hay que detener el proceso primero"
     else
-      rm -f ~/.log_usb.txt && echo "Se reinicio el log de puertos USB"
+        rm -f $FILE && echo "Se reinicio el log de puertos USB"
     fi
-    rm ~/.temp
+    rm $TEMP2
+elif [ "$1" = "--udev" -o "$1" = "-u" ]; then
+    echo -e "La configuracion de udev es la siguiente: \n"
+    cat $UDEV
+    echo -e "\nEl listado de puertos se encuentra en $UDEV"
 elif [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo "Este script registra los puertos USB que se conectan y desconectan
     [--run/-r]        Inicia el proceso de log. Ej: usb_log -r
-    [--kill/-k]       Detiene el proceso de log. Ej: usb_log -k
     [--show/-s]       Muestra el log de puertos USB. Ej: usb_log -s
     [--delete/-d]     Reinicia el log de puertos USB. Ej: usb_log -d
+    [--udev/-u]       Muestra la configuracion de udev. Ej: usb_log -u
     [--help/-h]       Muestra los comandos del script. Ej: usb_log help o usb_log -h
 
-    El listado de puertos se encuentra en ~/.log_usb.txt"
+    El listado de puertos se encuentra en $FILE"
     exit
 else
     echo -e "Comando invalido: Por favor escriba correctamente. \nEn caso de necesitar ayuda escriba: usb_log help o usb_log -h"
